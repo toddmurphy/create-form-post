c7493cb534cec1346c79b32229892200535396fa
toddmurphy <toddmurphy93@gmail.com>
2019-11-29 18:38:04 -0400

    initial component setup and form creation structure setup, basic styles for Form Creation

---
+++ new-form-post/src/components/FormSetup.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import CreateForm from './CreateForm';
+import Form from './Form';
+
+const FormSetup = () => {
+    return (
+        <div>
+            <h3>Initital form setup/ state- Form setup component</h3>
+            <CreateForm />
+            <Form />
+        </div>
+    )
+}
+
+export default FormSetup;
